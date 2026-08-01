05647dd8b694d025fa37cb56c2e78a12bc13d1a5
Chevdar <chaudar@evenbetgaming.com>
2026-08-01 22:03:35 +0300

    fix: publish package type declarations

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+import type { Linter } from 'eslint';
+
+export declare const base: Linter.Config[];
+export declare const typescript: Linter.Config[];
+export declare const typeChecked: Linter.Config[];
+export declare const stylistic: Linter.Config[];
+export declare const imports: Linter.Config[];
+export declare const recommended: Linter.Config[];
+
+export default recommended;
